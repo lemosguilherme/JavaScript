ab6d3129d0b96109db643037dc6936c6d811c722
Gui <152995074+lemosguilherme@users.noreply.github.com>
2024-01-01 10:32:01 -0300

    exercicio 2

---
+++ desafios/desafio001C/script.js
@@ -0,0 +1,10 @@
+function botao(){
+    var masculino = document.querySelector("#masculino")
+    var feminino = document.querySelector("#feminino")
+    if (masculino){
+        alert("masc")
+    }
+    else if (feminino){
+        alert("femin")
+    }
+}
